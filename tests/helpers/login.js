1596a111fb1692746c923e318831f7eacefba415
dvjhr <2dava.aditya21@gmail.com>
2025-05-14 01:33:53 +0700

    Modify login helper

--- tests/helpers/login.js
+++ tests/helpers/login.js
@@ -7,16 +7,29 @@
  */
 export async function loginWithCred(page, username, password) {
     let status = false;
-    await page.goto('/sign-in', {waitUntil: 'load'});
+
+    await page.goto('/sign-in', { waitUntil: 'load' });
+
     await page.getByRole('textbox', { name: 'Enter your email' }).click();
     await page.getByRole('textbox', { name: 'Enter your email' }).fill(username);
+
     await page.locator('div').filter({ hasText: /^PasswordForgot Password\?\*$/ }).locator('span').nth(1).click();
     await page.getByRole('textbox', { name: 'Enter your password' }).fill(password);
     await page.getByRole('button', { name: 'Sign In', exact: true }).click();
-    // if (await expect(page.locator('.ant-message-notice-content')).toBeVisible()) {
-    //     throw new Error("[LOGIN] Login failed!");
-    //     return status;
-    // }
-    status = true;
+
+    const modalMessage = page.locator(`//div[@class='ant-message']//span/..`)
+    await modalMessage.first().waitFor({ state: 'visible' });
+    
+    let modalMessageType = await modalMessage.locator('span').first().getAttribute('class');
+    if (modalMessageType.includes('anticon-close-circle')) {
+        console.log(`[DEBUG] ${await modalMessage.innerText()}`);
+        status = false;
+        throw new Error("[LOGIN] Login failed!");
+    } else if (modalMessageType.includes('anticon-check-circle')) {
+        console.log(`[DEBUG] ${await modalMessage.innerText()}`);
+        console.log(`[LOGIN] Login success!`);
+        status = true;
+    }
+    
     return status;
 }
