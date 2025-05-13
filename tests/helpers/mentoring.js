dad204dba02fa6e46d31ebdfe72e5af2d6b65bd0
dvjhr <2dava.aditya21@gmail.com>
2025-05-13 09:29:23 +0700

    Edit mentoring helpers

--- tests/helpers/mentoring.js
+++ tests/helpers/mentoring.js
@@ -8,21 +8,26 @@ import { expect } from '@playwright/test';
  * @param {string} tabText - Visible text of the tab to click
  */
 export async function clickAndCheckTab(page, tabText) {
-  const tab = page.getByRole('link', { name: tabText });
-  const highlightedClassRegex = /!text-tertiary-violet-50 bg-tertiary-violet-0/;
+    //   const tab = page.getByRole('link', { name: tabText });
+    const tab = page.locator(`//a[normalize-space(text()) = "${tabText}"]`);
+    const highlightedClassRegex = /!text-tertiary-violet-50 bg-tertiary-violet-0/;
 
-  let attempts = 0;
-  const maxAttempts = 3;
+    let attempts = 0;
+    const maxAttempts = 3;
 
-  while (attempts < maxAttempts) {
-    await tab.click();
-    attempts++;
-    try {
-      await expect(tab).toHaveClass(highlightedClassRegex, { timeout: 1000 });
-      return;
-    } catch {
+    while (attempts < maxAttempts) {
+        await tab.click();
+        attempts++;
+        try {
+            await expect(tab).toHaveClass(highlightedClassRegex, { timeout: 1000 });
+            return;
+        } catch {
+        }
     }
-  }
 
-  await expect(tab).toHaveClass(highlightedClassRegex);
+    console.log(tab.getAttribute('class'));
+    await expect(tab).toHaveClass(highlightedClassRegex);
+    if (attempts === maxAttempts && !tab.getAttribute('class').match(highlightedClassRegex)) {
+        console.error(`Tab "${tabText}" did not become highlighted after ${maxAttempts} attempts.`);
+    }
 }
